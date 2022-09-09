d5f3a3b5d91cb63a6bd9717d2a290efc56fc3baf
Amereen Hanvel <amereenhanvel786@gmail.com>
2022-09-08 20:37:22 -0400

    commit tree 2d5e3dfdc6404e4495bc948592f8b27ea69ef60b

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+main.remove()
+
+
+{/* <main id= "main"
+<h1> victory </h1>
+</main */}
+ 
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+
+newHeader.textContent = "Amereen is the champion"
